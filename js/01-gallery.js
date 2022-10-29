4322e0d2e831f85abbe1c5d1161e4fc69f379718
Mateusz Wójcikowski <yankes90@gmail.com>
2022-10-29 18:09:43 +0200

    HW upload

---
+++ js/01-gallery.js
@@ -0,0 +1,44 @@
+import { galleryItems } from './gallery-items.js';
+
+const gallery = document.querySelector('.gallery');
+const markup = galleryItems
+  .map(
+    image => `<div class="gallery__item">
+  <a class="gallery__link" href=${image.original}>
+    <img
+      class="gallery__image"
+      src=${image.preview}
+      data-source=${image.original}
+      alt="${image.description}"
+    />
+  </a>
+</div>`
+  )
+  .join('');
+
+gallery.insertAdjacentHTML('beforeend', markup);
+gallery.addEventListener('click', selectImg);
+
+function selectImg(event) {
+  event.preventDefault();
+
+  const dataSource = event.target.getAttribute('data-source');
+  const instance = basicLightbox.create(`
+    <img src= ${dataSource} width="853" height="1280">
+`);
+  
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  instance.show();
+
+  if (instance.visible()) {
+    document.addEventListener('keydown', event => {
+      if (event.key === 'Escape') {
+        instance.close();        
+      }
+      return;
+    });
+  }
+}
---
+++ js/02-lightbox.js
@@ -0,0 +1,18 @@
+import { galleryItems } from './gallery-items.js';
+
+
+const gallery = document.querySelector('.gallery');
+const markup = galleryItems
+  .map(
+    image => `<a class="gallery__item" href=${image.original}>
+  <img class="gallery__image" src=${image.preview} alt="${image.description}" />
+</a>`
+  )
+  .join('');
+gallery.insertAdjacentHTML('beforeend', markup);
+
+
+ (function () {
+   new SimpleLightbox('.gallery a', { captionDelay: 250, captionsData: 'alt' });    
+ })();
+
