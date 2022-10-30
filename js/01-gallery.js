0fe836e93aa452d6893c121e4a44b4d8d434d7e6
Mateusz Wójcikowski <yankes90@gmail.com>
2022-10-30 23:22:53 +0100

    Fix deleted unncesary listner creation

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,9 +1,9 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 
-const gallery = document.querySelector('.gallery');
+const gallery = document.querySelector(".gallery");
 const markup = galleryItems
   .map(
-    image => `<div class="gallery__item">
+    (image) => `<div class="gallery__item">
   <a class="gallery__link" href=${image.original}>
     <img
       class="gallery__image"
@@ -14,31 +14,40 @@ const markup = galleryItems
   </a>
 </div>`
   )
-  .join('');
+  .join("");
 
-gallery.insertAdjacentHTML('beforeend', markup);
-gallery.addEventListener('click', selectImg);
+gallery.insertAdjacentHTML("beforeend", markup);
+gallery.addEventListener("click", selectImg);
 
 function selectImg(event) {
   event.preventDefault();
 
-  const dataSource = event.target.getAttribute('data-source');
-  const instance = basicLightbox.create(`
+  const dataSource = event.target.getAttribute("data-source");
+  const instance = basicLightbox.create(
+    `
     <img src= ${dataSource} width="853" height="1280">
-`);
-  
-  if (event.target.nodeName !== 'IMG') {
+`,
+    {      
+      onClose: () => {
+        document.removeEventListener("keydown", closeInstance);        
+      },
+    }
+  );
+
+  if (event.target.nodeName !== "IMG") {
     return;
   }
 
   instance.show();
+  
+  function closeInstance(event) {
+    if (event.key === "Escape") {
+      instance.close();    
+    }
+    return;
+  }
 
   if (instance.visible()) {
-    document.addEventListener('keydown', event => {
-      if (event.key === 'Escape') {
-        instance.close();        
-      }
-      return;
-    });
+    document.addEventListener("keydown", closeInstance);    
   }
 }
